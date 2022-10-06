f00d7745052425c8835d0181e842cf6f202e9b36
Basit Ali <63063973+basitali111@users.noreply.github.com>
2022-10-06 22:31:19 +0500

    Adding one character code function

--- decodeMorceCode.rb
+++ decodeMorceCode.rb
@@ -1,1 +1,39 @@
 
+CODE = {
+    "A" => ".-", 
+    "B" => "-...",
+    "C" => "-.-.",
+    "D" => "-..",
+    "E" => '.',      
+    "F" => '..-.',
+    "G" => '--.',    
+    "H" => '....',   
+    "I" => '..',
+    "J" => '.---',   
+    "K" => '-.-',    
+    "L" => '.-..',
+    "M" => '--',
+    "N" => '-.',
+    "O" => '---',
+    "P" => '.--.',
+    "Q" => '--.-',
+    "R" => '.-.',
+    "S" => '...',
+    "T" => '-',
+    "U" => '..-',
+    "V" => '...-',
+    "W" => '.--',
+    "X" => '-..-',
+    "Y" => '-.--',
+    "Z" => '--..'
+}
+=begin
+Create a method to decode a Morse code character, takes a string parameter, 
+and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+=end
+def decode_char (morce_code)
+    @character = CODE.key(morce_code)
+    @character
+ end
+
+ puts decode_char ".-"
